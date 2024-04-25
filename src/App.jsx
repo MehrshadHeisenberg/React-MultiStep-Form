9c3fdbe0a9cc45088be5bbee51eadbd89b28c446
MehrshadHeisenberg3 <ytmehrshad@gmail.com>
2024-04-26 00:49:49 +0330

    Updated App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import "./index.css";
 
 function App() {
-  return (
-    <p className="text-6xl font-ubuntu font-medium">eoaasdsasd BITCHasd</p>
-  );
+  return <p className="text-6xl font-ubuntu font-medium">Hello There!</p>;
 }
 
 export default App;
